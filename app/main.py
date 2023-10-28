564c1557191e01c839a7f2433902b14991aede87
Vikas <vikas1618072@gmail.com>
2023-10-28 08:29:59 +0530

    correct schemas

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from app.schemas import CropInput
 import numpy as np
-import pandas as pd
 import pickle
 
 app = FastAPI(
@@ -9,6 +8,7 @@ app = FastAPI(
     description="Crop Recommendation is an application that provides personalized crop recommendations based on soil and environmental conditions. It helps farmers and agricultural enthusiasts make informed decisions on which crops to grow in a specific season. By inputting data on soil properties (N, P, K), temperature, humidity, pH, and rainfall, the application uses a machine learning model to predict the most suitable crops for cultivation. This project aims to optimize agricultural yield and promote sustainable farming practices.",
 )
 
+# Load the machine learning model from the pickle file
 with open("crop_recommendation.pkl", 'rb') as f:
     model = pickle.load(f)
 
@@ -19,18 +19,17 @@ def home():
 @app.post('/recommend')
 async def recommend(request: CropInput):
     try:
-        N = request.N
-        P = request.P
-        K = request.K
-        temperature = request.temperature
-        humidity = request.humidity
-        pH = request.pH
-        rainfall = request.rainfall
+        N = float(request.N)
+        P = float(request.P)
+        K = float(request.K)
+        temperature = float(request.temperature)
+        humidity = float(request.humidity)
+        pH = float(request.pH)
+        rainfall = float(request.rainfall)
 
         input_data = [[N, P, K, temperature, humidity, pH, rainfall]]
 
-        predict_crop = model.predict(input_data)
-        print(predict_crop)
-        return predict_crop[0]
+        predicted_crop = model.predict(input_data)
+        return {"recommended_crop": predicted_crop[0]}
     except Exception as e:
-        raise HTTPException(status_code=400, detail="Unable to make a recommendation.")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Unable to predict")
--- app/schemas.py
+++ app/schemas.py
@@ -1,23 +1,23 @@
 from pydantic import BaseModel
 
-
 class CropInput(BaseModel):
-    N = float
-    P = float
-    K = float
-    temperature = float
-    humidity = float
-    pH = float
-    rainfall = float
+    N: float
+    P: float
+    K: float
+    temperature: float
+    humidity: float
+    pH: float
+    rainfall: float
+
     class Config:
-        schema_extra = {
-            "example":{
-                "N":113,
-                "P":38,
-                "K":20,
-                "temperature":22.10718988,
-                "humidity":78.58320116,
-                "pH":25,
-                "rainfall":60
+        json_schema_extra = {
+            "example": {
+                "N": 113,
+                "P": 38,
+                "K": 20,
+                "temperature": 22.10718988,
+                "humidity": 78.58320116,
+                "pH": 25,
+                "rainfall": 60
             }
         }
